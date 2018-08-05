29c15c829a465b545955d9c350b0c18ba300a023
faramarz sadeghi <faramarz.sadeghi89@gmail.com>
2018-08-05 16:05:44 +0430

    set debug

--- src/Database/Migrations/2018_08_05_082135_create_slider_items_table.php
+++ src/Database/Migrations/2018_08_05_082135_create_slider_items_table.php
@@ -17,7 +17,6 @@ class CreateSliderItemsTable extends Migration
             $table->increments('id');
             $table->integer('slider_id')->unsigned()->default(0);
             $table->integer('item_id')->unsigned()->default(0);
-            $table->timestamps();
             $table->enum('is_active', array('1','2'))->default('1')->comment('0 equal to deactive and 1 is active');
             $table->integer('created_by')->unsigned()->default(0);
             $table->timestamps();
